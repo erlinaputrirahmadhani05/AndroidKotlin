45aa69b501afb06ffbcd578acdd7ce0e3c7e4458
asus\user <erlinarahmadhani05@gmail.com>
2024-02-26 21:57:30 +0700

    halo

--- app/src/main/java/com/example/androidkotlin/Hello.kt
+++ app/src/main/java/com/example/androidkotlin/Hello.kt
@@ -1,22 +1,31 @@
 package com.example.androidkotlin
 
-fun main(args: Array<String>) {
-    biodata("Erlina Putri Rahmadhani", "Madiun" 19)
-    hobby(nama1="Mendengarkan Musik"
-        desc1="Dengan mendengarkan musik bisa mengurangi stress dan membuat pikiran menjadi rileks")
+class Biodata(
+    val nama: String,
+    val TTL: String,
+    val umur: Int,
+    val alamat: String,
+    val hobi: List<String>
+) {
+    fun displayBiodata() {
+        println("===== BIODATA PRIBADI =====")
+        println("Nama: $nama")
+        println("TTL: $TTL")
+        println("Umur: $umur tahun")
+        println("Alamat: $alamat")
+        println("Hobi: ")
+        hobi.forEach { println("- $it") }
+    }
 }
-fun biodata(name=String, alamat=String, age=Int) {// minimal 2 parameter/args
-    println("===================================")
-    println("\t\tBIODATA")
-    println("===================================")
-    println("Nama\t:$name")
-    println("Usia\t:$age")
-    println("Alamat\t:$alamat")
-    println("===================================")
-}
-fun hobby(nama1:String, desc1:String){
-    println("\t\tHOBI SAYA")
-    println("===================================")
-    println("Saya memiliki hoby$nama1")
-    println("Alasan saya suka $nama1 adalah $desc1")
+
+fun main() {
+    val biodata = Biodata(
+        nama = "Erlina Putri Rahmadhani",
+        TTL = "Madiun, 05 Oktober 2005",
+        umur = 18,
+        alamat = "Ds. Ngengor, Kec.Pilangkenceng, Kab.Madiun",
+        hobi = listOf("mendengarkan musik", "bersepeda")
+    )
+
+    biodata.displayBiodata()
 }
